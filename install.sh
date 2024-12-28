a59193e7033ee899d0bd4a22ab3ba7ab27c22484
Marko Prodanovic <marko.prodanovic1@gmail.com>
2024-12-28 12:03:09 -0800

    Updated install script

--- install.sh
+++ install.sh
@@ -3,8 +3,22 @@
 # Create necessary directories
 mkdir -p ~/Library/Application\ Support/com.mitchellh.ghostty
 mkdir -p ~/.config
+mkdir -p ~/.tmux/plugins
+
+# Install TPM if it's not already installed
+TPM_PATH=~/.tmux/plugins/tpm
+if [ ! -d "$TPM_PATH" ]; then
+    echo "Installing TPM..."
+    git clone https://github.com/tmux-plugins/tpm "$TPM_PATH"
+else
+    echo "TPM already installed"
+fi
 
 # Create symbolic links
 ln -sf ~/dotfiles/ghostty/config ~/Library/Application\ Support/com.mitchellh.ghostty/config
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+
+# Install tmux plugins
+echo "Installing tmux plugins..."
+~/.tmux/plugins/tpm/bin/install_plugins
