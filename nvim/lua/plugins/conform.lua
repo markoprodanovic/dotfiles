9d29f693f7c6267e0e5c00be33f7c5a81ea7fc44
Marko Prodanovic <marko.prodanovic@amersports.com>
2026-04-01 15:27:20 -0700

    Stop unwanted php-cs-fixer from running

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         sh = {},
+        php = {},
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
         javascript = { "prettier" },
