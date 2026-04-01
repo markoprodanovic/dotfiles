d2559b46854a9110e8e93448e13d15abb8e6aa4e
Marko Prodanovic <marko.prodanovic1@gmail.com>
2026-03-31 21:08:08 -0700

    Toggle hidden/ignored files in picker

--- nvim/lua/plugins/picker.lua
+++ nvim/lua/plugins/picker.lua
@@ -1,13 +1,36 @@
 return {
   {
     "folke/snacks.nvim",
+    opts = {
+      picker = {
+        actions = {
+          toggle_hidden_ignored = function(picker)
+            picker.opts.hidden = not picker.opts.hidden
+            picker.opts.ignored = not picker.opts.ignored
+            picker:find()
+          end,
+        },
+        win = {
+          input = {
+            keys = {
+              ["<C-h>"] = { "toggle_hidden_ignored", mode = { "i", "n" } },
+            },
+          },
+          list = {
+            keys = {
+              ["<C-h>"] = { "toggle_hidden_ignored", mode = { "i", "n" } },
+            },
+          },
+        },
+      },
+    },
     keys = {
       {
         "<leader>ff",
         function()
-          Snacks.picker.files({ hidden = true, ignored = true })
+          Snacks.picker.files()
         end,
-        desc = "Find Files (incl. hidden & ignored)",
+        desc = "Find Files",
       },
     },
   },
