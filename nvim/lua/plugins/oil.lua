5daad94ed667c79c728063ca2db8cdbb5073e5c0
Marko Prodanovic <marko.prodanovic1@gmail.com>
2026-04-09 23:51:43 -0700

    Add mini.diff and oil.nvim

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    opts = {
+      default_file_explorer = false,
+    },
+    keys = {
+      { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+    },
+  },
+}
