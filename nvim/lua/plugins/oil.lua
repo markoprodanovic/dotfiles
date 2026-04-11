bfb11c2e4da20814eeb2a3462cabd6014837eb1a
Marko Prodanovic <marko.prodanovic@amersports.com>
2026-04-10 20:14:25 -0700

    Oil grep and find file in current dir keymaps

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,6 +4,22 @@ return {
     lazy = false,
     opts = {
       default_file_explorer = false,
+      keymaps = {
+        ["<leader>ff"] = {
+          callback = function()
+            local dir = require("oil").get_current_dir()
+            Snacks.picker.files({ cwd = dir, title = "Files: " .. vim.fn.fnamemodify(dir, ":.") })
+          end,
+          desc = "Find files in current directory",
+        },
+        ["<leader>sg"] = {
+          callback = function()
+            local dir = require("oil").get_current_dir()
+            Snacks.picker.grep({ cwd = dir, title = "Grep: " .. vim.fn.fnamemodify(dir, ":.") })
+          end,
+          desc = "Grep in current directory",
+        },
+      },
     },
     keys = {
       { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
