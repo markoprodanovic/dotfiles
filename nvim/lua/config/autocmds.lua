99a37ff326ce3984969091d872441427a26466af
Marko Prodanovic <marko.prodanovic1@gmail.com>
2026-04-11 15:50:09 -0700

    Disable markdown spellcheck

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,6 +7,16 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- LazyVim enables spell check for markdown (via lazyvim_wrap_spell augroup).
+-- This overrides it to disable spell check for markdown only, leaving other
+-- filetypes (text, plaintex, typst, gitcommit) unaffected.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
 vim.api.nvim_create_autocmd("VimLeave", {
   command = "set guicursor=a:ver25-blinkon1",
 })
