8e444412765e98d8d863fb17da287e37ee351bee
Marko Prodanovic <marko.prodanovic1@gmail.com>
2026-04-26 17:51:03 -0700

    Diffview colour fixes

--- nvim/lua/plugins/colourscheme.lua
+++ nvim/lua/plugins/colourscheme.lua
@@ -2,6 +2,26 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "night" },
+    opts = {
+      style = "night",
+      on_highlights = function(hl, c)
+        -- Diagonal fill: editor bg with blue-grey stripes
+        hl.DiffviewDiffDelete = { bg = c.bg, fg = c.dark3 }
+        -- File panel counts: text colour only, no background
+        hl.DiffviewFilePanelInsertions = { fg = c.git.add }
+        hl.DiffviewFilePanelDeletions = { fg = c.git.delete }
+        -- Status letters (M, A, D, etc.): text colour only, no background
+        hl.DiffviewStatusAdded       = { fg = c.git.add }
+        hl.DiffviewStatusUntracked   = { fg = c.git.add }
+        hl.DiffviewStatusModified    = { fg = c.git.change }
+        hl.DiffviewStatusRenamed     = { fg = c.git.change }
+        hl.DiffviewStatusCopied      = { fg = c.git.change }
+        hl.DiffviewStatusTypeChange  = { fg = c.git.change }
+        hl.DiffviewStatusUnmerged    = { fg = c.git.change }
+        hl.DiffviewStatusDeleted     = { fg = c.git.delete }
+        hl.DiffviewStatusUnknown     = { fg = c.git.delete }
+        hl.DiffviewStatusBroken      = { fg = c.git.delete }
+      end,
+    },
   },
 }
--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,12 +1,25 @@
 return {
   {
     "sindrets/diffview.nvim",
-    opts = {
-      view = {
-        merge_tool = {
-          layout = "diff3_mixed",
+    config = function()
+      require("diffview").setup({
+        view = {
+          merge_tool = {
+            layout = "diff3_mixed",
+          },
         },
-      },
-    },
+        keymaps = {
+          view = {
+            { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+          },
+          file_panel = {
+            { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+          },
+          file_history_panel = {
+            { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+          },
+        },
+      })
+    end,
   },
 }
