7c35fc91c850c2c1b343e017f9d8b2838141086c
Marko Prodanovic <marko.prodanovic@amersports.com>
2026-04-30 16:42:44 -0700

    Fix "yank path" keybinding for clean directory path with oil.nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,7 +10,13 @@ vim.keymap.set("v", "<C-_>", "gc", { remap = true, desc = "Toggle comment" })
 
 -- Copy the current file's path (relative to project root) to clipboard
 vim.keymap.set("n", "<leader>yp", function()
-  local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
+  local oil_ok, oil = pcall(require, "oil")
+  local path = oil_ok and oil.get_current_dir() or nil
+  if path then
+    path = vim.fn.fnamemodify(path, ":.")
+  else
+    path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
+  end
   vim.fn.setreg("+", path)
   vim.notify(path, vim.log.levels.INFO, { title = "Copied path" })
 end, { desc = "Yank file path (relative)" })
