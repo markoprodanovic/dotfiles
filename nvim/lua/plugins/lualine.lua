a61a915ed3b77659e38020c7baa830128309f54c
Marko Prodanovic <marko.prodanovic1@gmail.com>
2026-05-01 09:14:27 -0700

    Small Lualine modifications

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,15 +3,23 @@ return {
   opts = function(_, opts)
     local icons = LazyVim.config.icons
 
-    opts.options.section_separators   = { left = "", right = "" }
+    opts.options.section_separators = { left = "", right = "" }
     opts.options.component_separators = { left = "", right = "" }
     opts.options.theme = {
-      normal   = { a = { bg = "NONE", fg = "#DCD7BA", gui = "bold" }, b = { bg = "NONE", fg = "#DCD7BA" }, c = { bg = "NONE", fg = "#727169" } },
-      insert   = { a = { bg = "NONE", fg = "#98BB6C", gui = "bold" } },
-      visual   = { a = { bg = "NONE", fg = "#957FB8", gui = "bold" } },
-      replace  = { a = { bg = "NONE", fg = "#FF5D62", gui = "bold" } },
-      command  = { a = { bg = "NONE", fg = "#E6C384", gui = "bold" } },
-      inactive = { a = { bg = "NONE", fg = "#727169" }, b = { bg = "NONE", fg = "#727169" }, c = { bg = "NONE", fg = "#727169" } },
+      normal = {
+        a = { bg = "NONE", fg = "#DCD7BA", gui = "bold" },
+        b = { bg = "NONE", fg = "#DCD7BA" },
+        c = { bg = "NONE", fg = "#727169" },
+      },
+      insert = { a = { bg = "NONE", fg = "#98BB6C", gui = "bold" } },
+      visual = { a = { bg = "NONE", fg = "#957FB8", gui = "bold" } },
+      replace = { a = { bg = "NONE", fg = "#FF5D62", gui = "bold" } },
+      command = { a = { bg = "NONE", fg = "#E6C384", gui = "bold" } },
+      inactive = {
+        a = { bg = "NONE", fg = "#727169" },
+        b = { bg = "NONE", fg = "#727169" },
+        c = { bg = "NONE", fg = "#727169" },
+      },
     }
 
     opts.sections = {
@@ -48,8 +56,8 @@ return {
           },
         },
       },
-      lualine_y = { { "location", color = { fg = "#727169" } } },
-      lualine_z = { { "filetype", icon_only = true } },
+      lualine_y = {},
+      lualine_z = { { "location", color = { fg = "#727169" }, padding = { left = 1, right = 0 } } },
     }
 
     opts.inactive_sections = {
