1247a0d1aab231be6ffdf68a0139f7dea6a4a491
Marko Prodanovic <marko.prodanovic@amersports.com>
2026-06-05 14:04:05 -0700

    Dim tmux panes and neovim buffers that aren't currently in focus
    Makes it way easier to identify what pane/split/buffer is currently
    active

---
+++ nvim/lua/plugins/vimade.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "tadaa/vimade",
+    event = "VeryLazy",
+    opts = {
+      recipe = { "default", { animate = false } },
+      fadelevel = 0.6,
+      ncmode = "windows",
+      enablefocusfading = true,
+    },
+  },
+}
